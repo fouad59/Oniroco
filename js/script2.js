cd5c7070dc2f505627554b5ed80fc97e81d5b195
fouadej <eljilalifouad@gmail.com>
2021-06-01 18:00:36 +0200

    boostrap

--- js/script2.js
+++ js/script2.js
@@ -3,14 +3,14 @@ let categories = ['Teddie']
 let sectionMain = document.getElementById('sectionMain')
 // boucle  
 
-    fetch('http://localhost:3000/api/teddies')
+fetch('http://localhost:3000/api/teddies')
     .then(response => {
         return response.json()
     }).then(data =>{
-        let div = document.createElement("div")
         for(let x=0; x<data.length; x++){
-            div.appendChild (
-                '<img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/intro.jpg" alt="..." />'
+var div = document.createElement("div")
+            div.innerHTML =
+                '<img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src= " '+ data[x].imageUrl +' " alt="..." />'
                     + '<div class="intro-text left-0 text-center bg-faded p-5 rounded">'
                         + "<h2 class='section-heading mb-4'>"
                             +'<span class="section-heading-upper">' + data[x].name + '</span>'
@@ -19,7 +19,6 @@ let sectionMain = document.getElementById('sectionMain')
                         + '<p class="mb-3">' + data[x].description + '</p>'
                         + '<div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="teddies.html?id='+ data[x]._id +'">Voir le produit!</a></div>'
                     + '</div>'
-            )}
-    }).then( () => {
-        sectionMain.innerHTML = div
-    }) 
+sectionMain.appendChild(div)
+            }
+    })
